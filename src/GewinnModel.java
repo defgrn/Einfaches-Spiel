722535958bc4da56ba09aed90e5af802f19406ac
andreas <aoprea@student.tgm.ac.at>
2026-09-23 22:14:00 +0200

    implement round und random calculation logic

--- src/GewinnModel.java
+++ src/GewinnModel.java
@@ -24,4 +24,27 @@ public class GewinnModel {
         return rundenErgebnis;
     }
 
+    public void berechneComputerZahl() {
+        this.computerZahl = this.random.nextInt(9) + 1;
+    }
+
+    public void berechneRunde(int spielerZahl) {
+        this.spielerZahl = spielerZahl;
+        berechneComputerZahl();
+        int diff = Math.abs(this.spielerZahl - this.computerZahl);
+
+        switch (diff) {
+            case 0:
+                this.rundenErgebnis = 20;
+                break;
+            case 1:
+                this.rundenErgebnis = 5;
+                break;
+            default:
+                this.rundenErgebnis = -10;
+                break;
+        }
+        this.gesamtPunkte += this.rundenErgebnis;
+    }
+
 }
