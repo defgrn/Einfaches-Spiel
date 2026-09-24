e78c31c666a0596d80e60be23c806a6df9a7e03e
defgrn <andreas.oprea32@gmail>
2026-09-24 10:47:18 +0200

    implement GewinnController with event handling

---
+++ src/GewinnController.java
@@ -0,0 +1,58 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GewinnController {
+    private final GewinnModel model;
+    private final GewinnView view;
+
+    public GewinnController(GewinnModel model, GewinnView view) {
+        this.model = model;
+        this.view = view;
+
+        this.view.addSpielerEingabeListener(new SpielerEingabeListener());
+        this.view.addNochEinmalListener(new NochEinmalListener());
+    }
+
+    private class SpielerEingabeListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                int spielerZahl = Integer.parseInt(view.getSpielerEingabe());
+
+                if (spielerZahl < 1 || spielerZahl > 9) {
+                    view.setRundenErgebnis("Nur Zahlen von 1-9 ");
+                    return;
+                }
+                model.berechneRunde(spielerZahl);
+                view.setComputerZahl(model.getComputerZahl());
+                int diff = model.getRundenErgebnis();
+                String ergebnisText = (diff > 0 ? "+" : "") + diff;
+
+                if (model.hatVerloren()) {
+                    view.setRundenErgebnis("Verloren");
+                    view.setGesamtPunkte(String.valueOf(model.getGesamtPunkte()));
+                    view.setEditAktiv(false);
+                } else if (model.hatGewonnen()) {
+                    view.setRundenErgebnis("Gewonnen");
+                    view.setGesamtPunkte(String.valueOf(model.getGesamtPunkte()));
+                    view.setEditAktiv(false);
+                } else {
+                    view.setRundenErgebnis(ergebnisText);
+                    view.setGesamtPunkte(String.valueOf(model.getGesamtPunkte()));
+                }
+
+
+
+            } catch (NumberFormatException ex) {
+                view.setRundenErgebnis("Zahl eingeben");
+            }
+        }
+    }
+
+    private class NochEinmalListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            view.resetRunde();
+        }
+    }
+}
